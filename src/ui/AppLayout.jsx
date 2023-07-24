3412193d5979726b2e48ce929b6538d7b3efb998
Jami Islam Anik <anik.islam1494@gmail.com>
2023-07-24 12:32:12 +0600

    Add menu functionality
    In the menu page we see a list of pizza

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,21 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigation } from 'react-router-dom'
 import CartOverview from '../features/cart/CartOverview'
 import Header from './Header'
+import Loader from './Loader'
 
 function AppLayout() {
+    const navigation = useNavigation()
+
+    const isLoading = navigation.state === 'loading'
+
+    
+    console.log(navigation.state)
+
+
     return (
-        <div>
+        <div className='layout'>
+            {isLoading && <Loader />}
+
             < Header />
             <main>
                 <Outlet/>
