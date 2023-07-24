f150c0b47aab980580479a5c5023decc9e262d81
Jami Islam Anik <anik.islam1494@gmail.com>
2023-07-24 11:43:41 +0600

    initial commit

---
+++ src/ui/Header.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom"
+
+function Header() {
+    return (
+        <header>
+
+            <Link to='/'>Fast React Pizza Co.</Link>
+
+            <p>Header</p>
+        </header>
+    )
+}
+
+export default Header
